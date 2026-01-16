69e1ada7e85db75a49d46ed97099655c3ce756dc
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2026-01-16 10:55:51 -0700

    :truck: Move nemo plugin to plugins dir
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

---
+++ plugins/nemo/pii_detect_config/actions.py
@@ -0,0 +1,35 @@
+import logging
+import re
+from nemoguardrails.actions import action
+
+EMAIL_RE = re.compile(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}")
+SSN_RE = re.compile(r"\b\d{3}-\d{2}-\d{4}\b")
+
+logger = logging.getLogger(__name__)
+
+
+@action()
+def debug_log(value, label: str = "DEBUG"):
+    # Purposely high logger level for now to make sure this is visible
+    logger.warning(f"{label}: {value}")
+    return value
+
+@action()
+def detect_pii(tool_input):
+    """
+    Simple PII detector to start
+    """
+    text = str(tool_input)
+
+    findings = []
+
+    if EMAIL_RE.search(text):
+        findings.append("email")
+
+    if SSN_RE.search(text):
+        findings.append("ssn")
+
+    return {
+        "found": len(findings) > 0,
+        "types": findings,
+    }
